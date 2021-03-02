1d7b71a1ff49b853b5a36151c19047efec0301bf
joaopauloantunes <jpantunesdesouza@gmail.com>
2021-03-02 17:34:33 -0300

    checkpoint: data-nanogallery2 on html

--- galleriajs_project/app/scripts/index.js
+++ galleriajs_project/app/scripts/index.js
@@ -1,1 +1,34 @@
-console.log("script here");
+(function() {
+    Galleria.loadTheme('/galleria-1.6.1/dist/themes/folio/galleria.folio.min.js'); 
+    // Galleria.loadTheme('/galleria-1.6.1/dist/themes/azur/galleria.azur.min.js');
+    // Galleria.loadTheme('/galleria-1.6.1/dist/themes/twelve/galleria.twelve.min.js'); 
+    // Galleria.loadTheme('/galleria-1.6.1/dist/themes/fullscreen/galleria.fullscreen.min.js');
+    // Galleria.loadTheme('/galleria-1.6.1/dist/themes/classic/galleria.classic.min.js');
+    // Galleria.loadTheme('/galleria-1.6.1/dist/themes/miniml/galleria.miniml.min.js'); 
+    // Galleria.loadTheme('/galleria-1.6.1/dist/themes/'); 
+
+
+    Galleria.run('.galleria', {
+        // theme: 'folio',
+        // transition: 'fade',
+        // imageCrop: false,
+    });
+
+
+    /* Galleria.ready(function(options) {
+        // 'this' is the gallery instance
+        // 'options' is the gallery options
+        console.log(options);
+    
+        this.bind('image', function(e) {
+            Galleria.log('Now viewing ' + e.imageTarget.src);
+        });
+    }); */
+
+    /* Galleria.on('image', function(e) {
+        Galleria.log('Now viewing ' + e.imageTarget.src);
+    }); */
+
+
+    // $('.galleria').data('galleria').enterFullscreen();
+}());
---
+++ nanogallery2/app/scripts/index.js
@@ -0,0 +1,42 @@
+/* $(() => {
+    $("#nanogallery2").nanogallery2({
+        "itemsBaseURL": "/app/images/",
+        "items": [
+            {
+                src:          'picture_2.jpg',      // image url
+                srct:         'picture_2.jpg',      // thumbnail url
+                title:        'Title Picture 2',    // media title
+                description:  'Description 2'       // media description
+            }
+        ],
+        "thumbnailWidth": "auto",
+        "thumbnailHeight": "250",
+        "thumbnailBorderVertical": 1,
+        "thumbnailBorderHorizontal": 1,
+        "thumbnailLabel": {
+          "position": "overImageOnBottom",
+          "titleMultiLine": true,
+          "displayDescription": true,
+          "descriptionMultiLine": true
+        },
+        "allowHTMLinData": true,
+        "galleryDisplayMode": "pagination",
+        "galleryMaxRows": 2,
+        "galleryPaginationMode": "dots",
+        "thumbnailAlignment": "center",
+        "galleryFilterTags": "title",
+        "thumbnailLevelUp": true,
+        "thumbnailOpenImage": true,
+        "viewerToolbar":    {
+          "display":    true,
+          "standard":  "label",
+          "minimized": "label"
+        },
+          "viewerTools":      {
+            "topLeft":   "infoButton, pageCounter, custom1",
+            "topRight":  "playPauseButton, zoomButton, downloadButton, fullscreenButton, closeButton" 
+        },
+        "viewerTheme": "dark",
+        "viewerImageDisplay": "bestImageQuality"
+    });
+}); */
